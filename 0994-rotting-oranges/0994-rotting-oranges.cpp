f81604f211d2f6b360edf1929fdff0a9176a3f45
Mahek Advani <mahekadvani12@gmail.com>
2026-08-13 18:29:08 +0530

    Time: 0 ms (100%), Space: 16.7 MB (70.19%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,82 +2,63 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
 
-        int n = grid.size();
-        int m = grid[0].size();
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        int ans = 0;
+        queue<pair<int,int>> q;
+        int fresh = 0;
 
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        // Put ALL initially rotten oranges in queue
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
 
-        queue<pair<pair<int,int>, int>> q;
+                if (grid[r][c] == 2)
+                    q.push({r, c});
 
+                else if (grid[r][c] == 1)
+                    fresh++;
+            }
+        }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        int minutes = 0;
 
-                if(grid[i][j] == 2){
-                    q.push({{i,j},0});
-                    vis[i][j] = true;
+        int directions[4][2] = {
+            {-1, 0},  // up
+            {1, 0},   // down
+            {0, -1},  // left
+            {0, 1}    // right
+        };
+
+        while (!q.empty() && fresh > 0) {
+
+            int size = q.size();
+
+            // Process ONE complete minute
+            for (int i = 0; i < size; i++) {
+
+                auto [r, c] = q.front();
+                q.pop();
+
+                for (auto& dir : directions) {
+
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr >= 0 && nr < rows &&
+                        nc >= 0 && nc < cols &&
+                        grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+
+                        q.push({nr, nc});
+                    }
                 }
             }
+
+            minutes++;
         }
 
-
-        while(q.size() > 0){
-
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int time = q.front().second;
-
-            q.pop();
-
-            ans = max(ans,time);
-
-
-            // top
-            if(i-1 >= 0 && !vis[i-1][j] && grid[i-1][j] == 1){
-
-                q.push({{i-1,j},time+1});
-                vis[i-1][j] = true;
-            }
-
-
-            // right
-            if(j+1 < m && !vis[i][j+1] && grid[i][j+1] == 1){
-
-                q.push({{i,j+1},time+1});
-                vis[i][j+1] = true;
-            }
-
-
-            // bottom
-            if(i+1 < n && !vis[i+1][j] && grid[i+1][j] == 1){
-
-                q.push({{i+1,j},time+1});
-                vis[i+1][j] = true;
-            }
-
-
-            // left
-            if(j-1 >= 0 && !vis[i][j-1] && grid[i][j-1] == 1){
-
-                q.push({{i,j-1},time+1});
-                vis[i][j-1] = true;
-            }
-
-        }
-
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    return -1;
-                }
-            }
-        }
-
-
-        return ans;
+        return fresh == 0 ? minutes : -1;
     }
 };
